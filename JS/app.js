379726a072c68dd2b8c389c47624bf13cb31ee8d
ShoaibImran <simran3@bradford.ac.uk>
2022-01-26 16:06:14 +0000

    Refactored code + deleted unwanted files

--- JS/app.js
+++ JS/app.js
@@ -4,10 +4,10 @@ const { safeStringify } = require('./utilities');
 
 JSON.safeStringify = safeStringify;
 
-(async function test() {
+(async () => {
   try {
     let api = await SwaggerParser.validate(
-      'https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/resources/resource-manager/Microsoft.Resources/stable/2021-04-01/resources.json'
+      'https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/agrifood/resource-manager/Microsoft.AgFoodPlatform/preview/2020-05-12-preview/agfood.json'
     );
 
     try {
@@ -21,3 +21,4 @@ JSON.safeStringify = safeStringify;
     console.error(err);
   }
 })();
+
