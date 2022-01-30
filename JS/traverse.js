a12235b6e3539f97ae4809405d9a5e7c856f03c7
Yingbo Fu <fuyingbo1996@gmail.com>
2022-01-30 14:26:32 +0000

    Bug fix and add validation

--- JS/traverse.js
+++ JS/traverse.js
@@ -4,17 +4,28 @@
 'use strict';
 
 const converter = require('swagger2openapi');
+const validator = require('oas-validator');
 const fs = require('fs');
-let dir = "/Users/fuyingbo/Desktop/UCL/COMP0102 SASI/azure-rest-api-specs/specification";
-
-let options = {};
-options.r = true;
-options.resolveInternal = true;
-options.patch = true;
+let dir = "/Users/fuyingbo/Desktop/UCL/COMP0102 SASI/azure-rest-api-specs/specification/resources/resource-manager/Microsoft.Resources/stable";
 
 let convert = function(inputFile) {
-    options.output = inputFile.replace('.json', '-3.0.json');
+    let options = {};
+    options.r = true;
+    options.resolveInternal = true;
+    options.patch = true;
+    options.outfile = inputFile.replace('.json', '-3.0.json');
+    
     converter.convertFile(inputFile, options, function(err, options){
+        validator.validate(options.openapi, options)
+        .then(function(){
+          console.log(inputFile + " is Valid!");
+        })
+        .catch(function(err){
+            console.log(options.outfile + " is Invalid!");
+            console.warn(err.message);
+            if (options.context) console.warn('Location',options.context.pop());
+        });
+
         let s;
         try {
             s = JSON.stringify(options.openapi, null, options.indent||4);
@@ -23,10 +34,9 @@ let convert = function(inputFile) {
             console.warn('The result cannot be represented safely in the chosen output format');
             s = '{}';
         }
-        fs.writeFile(options.outfile, s, options.encoding || 'utf8');
+        fs.writeFileSync(options.outfile, s, options.encoding || 'utf8');
     });
 }
-
 
 let walk = function(dir) {
     let results = [];
@@ -39,7 +49,7 @@ let walk = function(dir) {
             results = results.concat(walk(file));
         } else {
             /* Is a file */
-            if (file.endsWith(".json")) results.push(file);
+            if (file.endsWith("resources.json")) results.push(file);
         }
     });
     return results;
