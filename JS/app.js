028d55bd4656d244d3a133da499cedb391c00db7
ArchawinWongkittiruk <willypoko@hotmail.com>
2022-02-01 17:55:13 +0000

    Clean up app.js

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,6 @@
 const SwaggerParser = require('@apidevtools/swagger-parser');
 const fs = require('fs');
 const converter = require('swagger2openapi');
-// const validator = require('oas-validator');
-
-let options = {};
-// options.resolve = true;
-options.outfile = '../example/convertedSpecExample.json';
-// options.patch = true;
-// options.source = inputFile;
 
 (async () => {
   try {
@@ -17,32 +10,19 @@ options.outfile = '../example/convertedSpecExample.json';
     );
 
     try {
-      fs.writeFileSync('../example/parsedSpecExample.json', JSON.stringify(api, null, 2));
+      fs.writeFileSync('../example/parsedSpecExample.json', JSON.stringify(api, null, 2), 'utf8');
     } catch (err) {
       console.error(err);
     }
 
-    converter.convertObj(api, options, function () {
-      // validator
-      //   .validate(options.openapi, options)
-      //   .then(function () {
-      //     console.log(options.outfile + " is Valid!");
-      //   })
-      //   .catch(function (err) {
-      //     console.log(options.outfile + " is Invalid!");
-      //     console.warn(err.message);
-      //     if (options.context) console.warn("Location", options.context.pop());
-      //   });
-
+    await converter.convertObj(api, (options = {}), function () {
       try {
         const converted = JSON.stringify(options.openapi, null, 2);
-        fs.writeFileSync(options.outfile, converted, options.encoding || 'utf8');
-      } catch (ex) {
-        console.warn('The result cannot be represented safely in the chosen output format');
+        fs.writeFileSync('../example/convertedSpecExample.json', converted, 'utf8');
+      } catch (err) {
+        console.error(err);
       }
     });
-
-    // await SwaggerParser.validate("../example/convertedSpecExample.json");
 
     console.log('API name: %s, Version: %s', api.info.title, api.info.version);
   } catch (err) {
