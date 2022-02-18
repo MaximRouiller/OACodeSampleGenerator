9c47e1f49aafadcfb9de7265e84132b81bc939b8
ArchawinWongkittiruk <willypoko@hotmail.com>
2022-02-18 16:24:22 +0000

    Pull out site sample writing from app to new file

---
+++ JS/writeSiteSample.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const generator = require('./index');
+
+(async () => {
+  // Pass in a specification URL as a third command line argument -> node app url
+
+  try {
+    const output = await generator(process.argv[2]);
+    const { apiInfo } = output;
+
+    console.log(`API name: ${apiInfo.title}, Version: ${apiInfo.version}`);
+
+    fs.writeFileSync(
+      `../azure-code-samples-site/samples/${apiInfo.title}_${apiInfo.version}.json`,
+      JSON.stringify(output, null, 2)
+    );
+  } catch (err) {
+    console.error(err);
+  }
+})();
