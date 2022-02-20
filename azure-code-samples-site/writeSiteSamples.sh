7a10e3f658ba0ec7d3da7ec4147bff76eb051f8a
Archawin Wongkittiruk <55857439+ArchawinWongkittiruk@users.noreply.github.com>
2022-02-20 22:36:09 +0000

    Remove instruction comment from writeSiteSamples

--- azure-code-samples-site/writeSiteSamples.sh
+++ azure-code-samples-site/writeSiteSamples.sh
@@ -1,5 +1,3 @@
-# Run from JS folder -> cd JS
-
 node writeSiteSample https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/advisor/resource-manager/Microsoft.Advisor/stable/2020-01-01/advisor.json
 node writeSiteSample https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/analysisservices/resource-manager/Microsoft.AnalysisServices/stable/2017-08-01/analysisservices.json
 node writeSiteSample https://raw.githubusercontent.com/Azure/azure-rest-api-specs/main/specification/resources/resource-manager/Microsoft.Resources/stable/2021-01-01/resources.json
