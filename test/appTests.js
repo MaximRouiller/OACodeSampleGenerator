9b6bf762f37c4ceac8bede4b90fcf556dd6d6baf
ShoaibImran <simran3@bradford.ac.uk>
2022-03-08 17:43:52 +0000

    deleted getCodeSnippet and formatted

--- test/appTests.js
+++ test/appTests.js
@@ -34,93 +34,92 @@ async function getRequestCodeSnippet(specURL) {
 it('Checks if spec is split into operations correctly', function () {});
 
 describe('Java request code generation', function () {
-
   it('Happy path (with args)', function () {
-    let snippet = getRequestCodeSnippet(SPEC_URL)
-    snippet.then(function(result) {
-        expect(result[0]).not.to.equal(null);
-        expect(result[0]).not.to.equal('');
-        expect(result[0]).to.be.an('string');
-        expect(result[0].length).to.be.at.least(300);
-    })  
+    let snippet = getRequestCodeSnippet(SPEC_URL);
+    snippet.then(function (result) {
+      expect(result[0]).not.to.equal(null);
+      expect(result[0]).not.to.equal('');
+      expect(result[0]).to.be.an('string');
+      expect(result[0].length).to.be.at.least(300);
+    });
   });
 
   it('Invalid args', function () {
-    let snippet = getRequestCodeSnippet("WRONG URL");
-    snippet.then(function(result) {
-        expect(result[0]).to.equal('');
-        expect(result[0]).not.to.be.an('string');
-        expect(result[0].length).not.to.be.at.least(300);
-    })  
+    let snippet = getRequestCodeSnippet('WRONG URL');
+    snippet.then(function (result) {
+      expect(result[0]).to.equal('');
+      expect(result[0]).not.to.be.an('string');
+      expect(result[0].length).not.to.be.at.least(300);
+    });
   });
 
   it('Null args', function () {
-    let snippet = getRequestCodeSnippet(null)
-    snippet.then(function(result) {
-        expect(result[0]).to.equal('');
-        expect(result[0]).not.to.be.an('string');
-        expect(result[0].length).not.to.be.at.least(300);
-    })
+    let snippet = getRequestCodeSnippet(null);
+    snippet.then(function (result) {
+      expect(result[0]).to.equal('');
+      expect(result[0]).not.to.be.an('string');
+      expect(result[0].length).not.to.be.at.least(300);
+    });
   });
 });
 
 describe('Python request code generation', function () {
   it('Happy path (with args)', function () {
-    let snippet = getRequestCodeSnippet(SPEC_URL)
-    snippet.then(function(result) {
-        expect(result[1]).not.to.equal(null);
-        expect(result[1]).not.to.equal('');
-        expect(result[1]).to.be.an('string');
-        expect(result[1].length).to.be.at.least(300);
-    })
+    let snippet = getRequestCodeSnippet(SPEC_URL);
+    snippet.then(function (result) {
+      expect(result[1]).not.to.equal(null);
+      expect(result[1]).not.to.equal('');
+      expect(result[1]).to.be.an('string');
+      expect(result[1].length).to.be.at.least(300);
+    });
   });
 
   it('Invalid args', function () {
-    let snippet = getRequestCodeSnippet("WRONG URL");
-    snippet.then(function(result) {
-        expect(result[0]).to.equal('');
-        expect(result[0]).not.to.be.an('string');
-        expect(result[0].length).not.to.be.at.least(300);
-    })
+    let snippet = getRequestCodeSnippet('WRONG URL');
+    snippet.then(function (result) {
+      expect(result[0]).to.equal('');
+      expect(result[0]).not.to.be.an('string');
+      expect(result[0].length).not.to.be.at.least(300);
+    });
   });
 
   it('Null args', function () {
-    let snippet = getRequestCodeSnippet(null)
-    snippet.then(function(result) {
-        expect(result[0]).to.equal('');
-        expect(result[0]).not.to.be.an('string');
-        expect(result[0].length).not.to.be.at.least(300);
-    })
+    let snippet = getRequestCodeSnippet(null);
+    snippet.then(function (result) {
+      expect(result[0]).to.equal('');
+      expect(result[0]).not.to.be.an('string');
+      expect(result[0].length).not.to.be.at.least(300);
+    });
   });
 });
 
 describe('C# request code generation', function () {
   it('Happy path (with args)', function () {
-    let snippet = getRequestCodeSnippet(SPEC_URL)
-    snippet.then(function(result) {
-        expect(result[0]).not.to.equal(null);
-        expect(result[0]).not.to.equal('');
-        expect(result[0]).to.be.an('string');
-        expect(result[0].length).to.be.at.least(300);
-    })
+    let snippet = getRequestCodeSnippet(SPEC_URL);
+    snippet.then(function (result) {
+      expect(result[0]).not.to.equal(null);
+      expect(result[0]).not.to.equal('');
+      expect(result[0]).to.be.an('string');
+      expect(result[0].length).to.be.at.least(300);
+    });
   });
 
   it('Invalid args', function () {
-    let snippet = getRequestCodeSnippet(SPEC_URL)
-    snippet.then(function(result) {
+    let snippet = getRequestCodeSnippet(SPEC_URL);
+    snippet.then(function (result) {
       expect(result[0]).to.equal('');
       expect(result[0]).not.to.be.an('string');
       expect(result[0].length).not.to.be.at.least(300);
-  })
+    });
   });
 
   it('Null args', function () {
-    let snippet = getRequestCodeSnippet(null)
-    snippet.then(function(result) {
-        expect(result[0]).to.equal('');
-        expect(result[0]).not.to.be.an('string');
-        expect(result[0].length).not.to.be.at.least(300);
-    })
+    let snippet = getRequestCodeSnippet(null);
+    snippet.then(function (result) {
+      expect(result[0]).to.equal('');
+      expect(result[0]).not.to.be.an('string');
+      expect(result[0].length).not.to.be.at.least(300);
+    });
   });
 });
 
